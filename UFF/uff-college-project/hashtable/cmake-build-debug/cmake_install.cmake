ba19f4ae53e8a1b52f36f057fb7a2a4ef3e6bcb1
Carlos Eduardo <carlos.eduardo.cs@icloud.com>
2023-07-01 18:47:13 -0300

    fix: correção de código e melhoria de projeto

--- UFF/uff-college-project/hashtable/cmake-build-debug/cmake_install.cmake
+++ UFF/uff-college-project/hashtable/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/cadu/Documentos/Projetos/hashtable
+# Install script for directory: /home/cadu/Documentos/GitHub/projects-and-studies-cpp/UFF/uff-college-project/hashtable
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/cadu/Documentos/Projetos/hashtable/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/cadu/Documentos/GitHub/projects-and-studies-cpp/UFF/uff-college-project/hashtable/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
